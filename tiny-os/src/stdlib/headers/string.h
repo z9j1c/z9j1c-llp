340f372dbb93bf9202bf1ff86ee202d259a7451d
z9j1c <ergycjc@gmail.com>
2020-12-01 20:58:52 +0300

    Make actualized, memcmp added

--- tiny-os/src/stdlib/headers/string.h
+++ tiny-os/src/stdlib/headers/string.h
@@ -8,3 +8,5 @@ size_t strlen( const char* str );
 void* memcpy( void* dest, const void* src, size_t num );
 
 void* memset ( void * ptr, int value, size_t num );
+
+int memcmp( const void* str1, const void* str2, size_t count );
--- tiny-os/src/stdlib/src/string.c
+++ tiny-os/src/stdlib/src/string.c
@@ -29,3 +29,15 @@ void* memset ( void* ptr, int value, size_t num ) {
     }
     return ptr;
 }
+
+int memcmp( const void* str1, const void* str2, size_t count ) {
+    const unsigned char *s1 = (const unsigned char*)str1;
+    const unsigned char *s2 = (const unsigned char*)str2;
+
+    while (count-- > 0) {
+        if (*s1++ != *s2++) {
+            return s1[-1] < s2[-1] ? -1 : 1;
+        }
+    }
+    return 0;
+}
