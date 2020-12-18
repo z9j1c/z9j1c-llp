5836cdfd2e00c0b1eeebe5fcaa014b366344920b
z9j1c <ergycjc@gmail.com>
2020-12-18 18:16:49 +0300

    ASM: inb function

--- tiny-os/src/stdlib/headers/asm.h
+++ tiny-os/src/stdlib/headers/asm.h
@@ -4,3 +4,9 @@
 static inline void outb(uint16_t port, uint8_t data) {
   asm volatile("out %0,%1" : : "a" (data), "d" (port));
 }
+
+static inline uint8_t inb(uint16_t port) {
+  uint8_t data;
+  asm volatile("in %1,%0" : "=a" (data) : "d" (port));
+  return data;
+}
